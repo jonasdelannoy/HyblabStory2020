c03d6addce3892369ac13326fc7057df990b7791
yahuan <yahuanc@gmail.com>
2020-02-06 17:30:50 +0100

    feedback page; need image alignment

---
+++ sorties-de-secours/public/js/feedback.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("#btnSend").click(function(){
+   /*TODO: Envoie valeurs obtenues de la page
+   $("#textTel").text()
+   $("#textMail").text()
+   $("#textNom").text()
+   $("#textPrenom").text()
+   $("#textObject").text()
+   $("#textMessage").text()
+
+   */
+    alert("simuler Envoyé");
+  });
+
+
+});
